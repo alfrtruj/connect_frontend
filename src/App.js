73a3bb6ea320e0c2236f4ede0f4199294a56fda5
Alfredo Trujillo <alfredo.trujillo@piql.com>
2021-02-25 21:54:57 +0100

    add css styles to navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>myConnect</h1>
+      <div className="content">
+        
+      </div>
+      
     </div>    
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import logo from './connect_logo.png';
+import react_logo from './react_logo.png';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <img src={logo} className="logo" alt="Connect logo"/>
+            <div className="logos">
+                <img src={logo} className="logo" alt="Connect logo"/>
+                <img src={react_logo} className="react_logo" alt="React logo"/>
+            </div>
             <div className="links">
                 <a href="/">Overview</a>
                 <a href="/ingest">Ingest</a>
